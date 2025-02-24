3ee62b229cf581e1cab3e5a89800b8c541e5fcbf
Shivani <jaiswalshivani013@gmail.com>
2025-02-24 09:57:27 +0000

    Commiting to main

--- reader.c
+++ reader.c
@@ -6,12 +6,23 @@
 int main(int argc,char* argv[])
 {
     printf("Opening the file which writer created\n");
-    int a=open("sample_write.txt",O_RDONLY);
+    int a=open("sample_write.txt",O_RDWR);
     printf("File got opened and file handler created\n");
-    int y;
+    int brr[10];
     printf("File start reading\n");
-    int b=read(a,&y,sizeof(y));
-    printf("Read from writer file,%d\n",y);
+    int b=read(a,brr,sizeof(int)*10);
+    for(int i=0;i<10;i++)
+    {
+    printf("Read from writer file %d\n",brr[i]);
+    }
+    int sum=0;
+    for(int i=0;i<10;i++)
+    {
+        sum=sum+brr[i];
+    }
+    printf("Sending sum to writer %d",sum);
+    int c=write(a,&sum,sizeof(int));
     close(a);
+    
     return 0;
 }
--- writer.c
+++ writer.c
@@ -1,12 +1,16 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<fcntl.h>
+#include<stdio.h> //for printf
+#include<unistd.h> //for close
+#include<sys/types.h>// for open
+#include<sys/stat.h> //for open
+#include<fcntl.h>  //for open,write and read function and for wronly
+#include<stdlib.h> //for random integer
+#include<time.h> //for time
 int main(int argc,char* argv[])
 {
+  int arr[10];
+   srand(time(NULL));
     printf("Opening the file \n");
-    int a= open("sample_write.txt",O_WRONLY);
+    int a= open("sample_write.txt",O_RDWR);
     if(a == -1)
     {
         printf("Unable to open\n");
@@ -14,10 +18,21 @@ int main(int argc,char* argv[])
     else
     {
       printf("Trying to write \n");
-    int x=45;
-    int b=write(a,&x,sizeof(x));
-      printf("Written value %d to the file\n",x);
+    for(int i=0;i<10;i++)
+    {
+       arr[i]=rand()%100;
     }
+    int b=write(a,arr,sizeof(int)*10);
+      printf("Written 10 numbers to array\n");
+    
+    }
+    for(int i=0;i<10;i++){
+    printf("Sending number %d\n",arr[i]);
+    }
+    int num=0;
+    int c=read(a,&num,sizeof(int));
+    printf("Got sum from reader %d\n",num);
     close(a);
+
     return 0;
 }
