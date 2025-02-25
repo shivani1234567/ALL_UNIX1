b785f4956fb485ab31555002090416fed6b22486
Shivani <jaiswalshivani013@gmail.com>
2025-02-25 17:38:28 +0000

    Commiting exec system call

---
+++ execl.c
@@ -0,0 +1,24 @@
+#include<unistd.h>
+#include<stdio.h>
+int main(int argc,char*argv[])
+{
+    int id=fork();
+    if(id==0){
+
+    printf("Before \n");
+    execl("/bin/ping","ping proess","-c","1","google.com",NULL);//pinging to infinite times
+    for(int i=0;i<100;i++)
+    {
+    printf("After\n");
+    }
+
+   }
+   else{
+     sleep(1);
+     printf("Hi From parent \n");
+
+
+   }
+
+    return 0;
+}
